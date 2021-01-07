f9b2dfcfbfd9ab3e2a7da00f7a78eef34546e785
Diego Sousa <diegosous@gmail.com>
2021-01-06 22:37:15 -0300

    POST New Product

--- backend/src/main/java/br/dev/diego/dsdelivery/controllers/ProductController.java
+++ backend/src/main/java/br/dev/diego/dsdelivery/controllers/ProductController.java
@@ -1,13 +1,14 @@
 package br.dev.diego.dsdelivery.controllers;
 
+import br.dev.diego.dsdelivery.dto.OrderDTO;
 import br.dev.diego.dsdelivery.dto.ProductDTO;
 import br.dev.diego.dsdelivery.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,5 +22,12 @@ public class ProductController {
     public ResponseEntity<List<ProductDTO>> findAll(){
         List<ProductDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
--- backend/src/main/java/br/dev/diego/dsdelivery/services/ProductService.java
+++ backend/src/main/java/br/dev/diego/dsdelivery/services/ProductService.java
@@ -1,13 +1,17 @@
 package br.dev.diego.dsdelivery.services;
 
+import br.dev.diego.dsdelivery.dto.OrderDTO;
 import br.dev.diego.dsdelivery.dto.ProductDTO;
+import br.dev.diego.dsdelivery.entities.Order;
 import br.dev.diego.dsdelivery.entities.Product;
+import br.dev.diego.dsdelivery.entities.enums.OrderStatus;
 import br.dev.diego.dsdelivery.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,6 +25,13 @@ public class ProductService {
     public List<ProductDTO> findAll(){
         List<Product> list = repository.findAllByOrderByNameAsc();
         return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto){
+        Product product = new Product(null, dto.getName(), dto.getPrice(), dto.getDescription(), dto.getImageUri());
+        product = repository.save(product);
+        return new ProductDTO(product);
     }
 
 }
