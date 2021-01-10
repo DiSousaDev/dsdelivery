161dc7c551470d2f7328c540bbe465df468b41f5
Diego Sousa <diegosous@gmail.com>
2021-01-09 21:26:33 -0300

    Adjust enviroment variables

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {OrderPayLoad} from "./pages/Orders/types";
 
-const API_URL = 'https://diegodev-dsdelivery.herokuapp.com';
+const API_URL = process.env.REACT_APP_API_URL;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAPBOX;
 
 export function fetchProducts() {
